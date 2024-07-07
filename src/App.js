d3ccf43e018383eb16fc7991e8eab77c5c235b8e
Yashgit1203 <yg63852@gmail.com>
2024-07-07 20:21:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import MainContent from "./components/MainContent";
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
 import Add from "./components/Add";
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
 const App = () => {
-  const [group,setgroup] = useState(false);
-  const onclick =()=> {
-      setgroup(!group)
-      
-  }
+  const [group, setGroup] = useState(false);
+  const onClick = () => {
+    setGroup(!group);
+  };
+
   return (
     <Router>
-    
-    <div className="App ">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid header-nav d-flex justify-content-between">
-        <Navbar />
-        <button class="navbar-toggler btn-re"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon " ></span>
-    </button>
-    <div class="collapse  navbar-collapse" id="navbarNavDropdown">
-      <div className="collapse-re">
-      <div className="col-lg-4 h-form col-md-12 col-sm-12 d-flex flex-column justify-content-center h-100" >
-         <Form group={group} onclick={onclick} />
-        </div>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid header-nav d-flex justify-content-between">
+            <Navbar />
+            <button
+              className="navbar-toggler btn-re"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <div className="collapse-re">
+                <div className="col-lg-4 h-form col-md-12 col-sm-12 d-flex flex-column justify-content-center h-100">
+                  <Form group={group} onClick={onClick} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
-    </div>
-      </div>
-      </nav>
-    </div>
-    <Routes>
-    <Route path="/add" element={<Add/>}/>
-    <Route path="/home" element={<MainContent />}/>
-    </Routes>
+      <Routes>
+        <Route path="/add" element={<Add />} />
+        <Route path="/home" element={<MainContent />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+      </Routes>
     </Router>
   );
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,10 +12,10 @@ const Hero = ({group,onclick}) => {
             <div className="h_title py-5">
               <h4 className='fw-bold fst-italic fs-1'>Explore your <span style={{color:"#0096C8"}}>hobby</span> or <span style={{color:"#8064A2"}}>passion</span></h4>
             </div>
-            <div className="h_main me-5">
+            <div className="h_main me-5 " style={{fontSize:"1.15rem"}}>
               <p className='lh-lg me-5'>Sign-in to interact with a community of fellow hobbyists and an eco-system of experts, teachers, suppliers, classes, workshops, and places to practice, participate or perform. <span className='h_btext'>Your hobby may be about visual or performing arts, sports, games, gardening, model making, cooking, indoor or outdoor activities…</span></p>
             </div>
-            <div className="h_footer me-5">
+            <div className="h_footer me-5" style={{fontSize:"1.15rem"}}>
               <p className='lh-lg me-5 h_btext'>If you are an expert or a seller, you can Add your Listing and promote yourself, your students, products,  services or events.  Hop on your hobbyhorse and enjoy the ride.</p>
             </div>
           </div>
